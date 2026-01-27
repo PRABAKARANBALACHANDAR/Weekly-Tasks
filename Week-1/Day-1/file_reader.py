992dd65a5addc38f14e086c3dcaa73761af20b30
Prabakaran B <166287610+PRABAKARANBALACHANDAR@users.noreply.github.com>
2026-01-27 17:49:44 +0530

    Delete Week-1 directory

--- Week-1/Day-1/file_reader.py
+++
@@ -1,8 +0,0 @@
-filename = 'business_51.txt'
-
-with open(filename, 'r') as file:
-    content = file.read()
-line_count = content.count('\n') + 1 if content else 0
-word_count = len(content.split())  
-print(f"Number of lines: {line_count}")
-print(f"Number of words: {word_count}")
--- Week-1/Day-2/email_validator.py
+++
@@ -1,7 +0,0 @@
-import re
-pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-input_email = input("Enter your email address: ")
-if re.match(pattern, input_email):
-    print("Valid email address")   
-else:
-    print("Invalid email address")
